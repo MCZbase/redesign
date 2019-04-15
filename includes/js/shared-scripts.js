c43ae9c302355808cc3ace70b1dd0fc185c9a2a7
Michelle Kennedy <mkennedy01@fas.harvard.edu>
2019-04-15 13:25:06 -0400

    Scripts at time of specimens page freeze

---
+++ includes/js/shared-scripts.js
@@ -0,0 +1,15 @@
+$(function () {
+            $("#popDialog").dialog({
+                autoOpen: false,
+                width: 650,
+                height: 600,
+            });
+            $(_btnToDialog).click(function () {
+                $("#popDialog").dialog("open");
+            });
+        });
+    });
+
+$(document).on("click", "button.popperbtn", function() {
+    $("#popDialog").dialog("open");
+});
